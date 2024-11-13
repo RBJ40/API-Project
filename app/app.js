c5a49f1e4094332a08a441b40fd817d02dbecca8
RBJ40 <celinechan268@gmail.com>
2024-11-13 12:22:57 -0500

    draft

---
+++ app/app.js
@@ -0,0 +1,19 @@
+import "./style.css";
+//gets data
+//shows data
+async function getData() {
+  try {
+    const response = await fetch(); //fetch returns promise/ paste api link as string in quotation marks
+    //guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    alert("Sorry. Could not find data requested.");
+  }
+}
+getData();
